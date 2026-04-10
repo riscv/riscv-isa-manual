a1062c31248649683c89163ce813e42e0336f658
Andrew Waterman <andrew@sifive.com>
2026-04-10 13:00:49 -0700

    WIP

--- src/lib/macros.rb
+++ src/lib/macros.rb
@@ -1,53 +1,69 @@
 require 'asciidoctor'
 require 'asciidoctor/extensions'
 
+# Macro                 Expands to
+#
+# insn:foo[]            FOO
+# insn:foo[bar,baz]     FOO bar, baz
+#
+# ext:zoo[]             `Zoo`
+#
+# extlink:zoo[]         <<ext:zoo, `Zoo`>>
+#
+# csr:foo[]             `foo`
+# csr::[bar]            `BAR`
+# csr:foo[bar]          `foo.BAR`
+#
+# csrlink:foo[]         <<csr:foo, `foo`>>
+# csrlink:foo[bar]      <<csr:foo_bar, `foo.BAR`>>
+
+module RVFormat
+  def self.instruction(name, args)
+    name.upcase + (args.any? ? "\u{a0}" : "") + args.values.join(",\u{a0}")
+  end
+  
+  def self.extension(name)
+    "`#{name.capitalize}`"
+  end
+  
+  def self.csr(name, field)
+    name = name.empty? ? [] : [name.downcase]
+    field = field.empty? ? [] : [field.upcase]
+    "`#{(name + field).join(".\u{2060}")}`"
+  end
+end
+
 Asciidoctor::Extensions.register do
   inline_macro :insn do
-    # convert insn:foo[] to FOO
-    # convert insn:foo[bar,baz] to FOO bar, baz
     process do |parent, name, args|
-      str = name.upcase
-
-      if args.any?
-        str = "#{str}\u{a0}#{args.values.join(",\u{a0}")}"
-      end
-
-      create_inline parent, :quoted, str
+      create_inline parent, :quoted, parent.apply_subs(RVFormat.instruction(name, args), [:quotes])
     end
   end
 
   inline_macro :ext do
-    # convert ext:foo[] to `Foo`
     process do |parent, name, args|
-      unless args.empty?
+      if args.any?
         parent.document.logger.fatal "macro ext:#{name}[] does not accept arguments"
       end
 
-      str = "`#{name.capitalize}`"
-      str = parent.apply_subs(str, [:quotes])
-
-      create_inline parent, :quoted, str
+      create_inline parent, :quoted, parent.apply_subs(RVFormat.extension(name), [:quotes])
     end
   end
 
   inline_macro :csr do
-    # convert csr:foo[] to `foo`
-    # convert csr::[bar] to `BAR`
-    # convert csr:foo[bar] to `foo.BAR`
     process do |parent, name, args|
       str = name == ":" ? "" : "#{name.downcase}"
 
-      if args.size == 1
-        str += ".\u{2060}" unless str.empty?
-        str += args[1].upcase
-      elsif args.size > 1
+      if args.size > 1
         parent.document.logger.fatal "macro csr:#{name}[] takes at most one argument"
+      elsif name == ":" && args.empty?
+        parent.document.logger.fatal "macro csr::[] takes exactly one argument"
       end
 
-      str = "`#{str}`"
-      str = parent.apply_subs(str, [:quotes])
+      name = name == ":" ? "" : name
+      field = args.fetch(1, "")
 
-      create_inline parent, :quoted, str
+      create_inline parent, :quoted, parent.apply_subs(RVFormat.csr(name, field), [:quotes])
     end
   end
 end
