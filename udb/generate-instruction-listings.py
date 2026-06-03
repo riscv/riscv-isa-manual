95b50510d1f11af49eadb6d27f3017d474c13d8f
Andrew Waterman <andrew@sifive.com>
2026-06-02 17:58:33 -0700

    tmp
    Signed-off-by: Andrew Waterman <andrew@sifive.com>

--- udb/generate-instruction-listings.py
+++ udb/generate-instruction-listings.py
@@ -15,31 +15,39 @@ def build_database(files):
 
     return db
 
-def extensions_defined_in(inst):
-    def helper(x):
-        key = next(iter(x))
-        value = x[key]
-        match key:
-            case 'name':
-                return [value]
-            case 'extension':
+def traverse_extensions(x, name):
+    key = next(iter(x))
+    value = x[key]
+    match key:
+        case 'name':
+            return [value]
+        case 'extension':
+            return helper(value)
+        case 'anyOf' | 'oneOf':
+            return [item for sublist in map(helper, value) for item in sublist]
+        case 'allOf':
+            value = {k: v for d in x[key] for k, v in d.items()}
+            if len(value) == 2 and 'xlen' in value and 'extension' in value:
+                res = helper(value['extension'])
+                return list(map(lambda x: f"{x} (RV{value['xlen']})", res))
+            if len(value) == 2 and 'not' in value and 'name' in value:
                 return helper(value)
-            case 'anyOf' | 'oneOf':
-                return [item for sublist in map(helper, value) for item in sublist]
-            case 'allOf':
-                value = {k: v for d in x[key] for k, v in d.items()}
-                if len(value) == 2 and 'xlen' in value and 'extension' in value:
-                    res = helper(value['extension'])
-                    return list(map(lambda x: f"{x} (RV{value['xlen']})", res))
-                if len(value) == 2 and 'not' in value and 'name' in value:
-                    return helper(value)
-                if len(value) == 1 and 'name' in value:
-                    return helper(value)
-                raise Exception(f"{inst['name']} has unrecognized 'allOf' pattern {value}")
-            case _:
-                raise Exception(f"{inst['name']} has unknown 'definedBy' key {key}")
+            if len(value) == 1 and 'name' in value:
+                return helper(value)
+            raise Exception(f"{name} has unrecognized 'allOf' pattern {value}")
+        case _:
+            raise Exception(f"{name} has unknown key {key}")
 
-    return helper(inst['definedBy'])
+def extensions_implied_by(ext):
+    if 'requirements' not in ext:
+        return []
+    return traverse_extensions(ext['requirements'], f"{ext['name']}.requirements")
+
+def extensions_defined_in(inst, xlen):
+    return traverse_extensions(inst['definedBy'], f"{inst['name']}.definedBy")
+
+def extensions_defined_in(inst):
+    return traverse_extensions(inst['definedBy'], f"{inst['name']}.definedBy")
 
 def main():
     parser = argparse.ArgumentParser()
@@ -53,9 +61,12 @@ def main():
     args = parser.parse_args()
 
     db = build_database(args.yamls)
+    print(extensions_implied_by(db['extension']['Zca']))
+    print(extensions_implied_by(db['extension']['C']))
+    #print(extensions_dependent_on('Zca', db['extension']))
 
-    for inst in db['instruction'].values():
-        print(inst['name'] + " " + str(extensions_defined_in(inst)))
+    #for inst in db['instruction'].values():
+    #    print(inst['name'] + " " + str(extensions_defined_in(inst)))
 
 if __name__ == "__main__":
     main()
